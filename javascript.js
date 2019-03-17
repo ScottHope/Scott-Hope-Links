106079352057fe400724a1909db88edc743f94e6
ScottHope <hughegreen@btinternet.com>
2019-03-17 23:42:37 +0000

    new buttons and search

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
+let search = document.querySelector("input");
+let buttons = document.querySelectorAll(".button");
+search.addEventListener("keyup", buttonSearch);
 
+function buttonSearch(){
+
+buttons.forEach(function(a){
+
+if(a.textContent.toLowerCase().search(search.value.toLowerCase()) === -1) {a.classList.add("invalid")}else{a.classList.remove("invalid")}
+console.log(a);
+
+})}
