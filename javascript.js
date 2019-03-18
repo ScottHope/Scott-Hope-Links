cb819f2f3b2a4af7d75de97ef387a476242cb313
ScottHope <hughegreen@btinternet.com>
2019-03-18 12:06:03 +0000

    changed search button type

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 let search = document.querySelector("input");
 let buttons = document.querySelectorAll(".button");
-search.addEventListener("keyup", buttonSearch);
+search.addEventListener("input", buttonSearch);
 
 function buttonSearch(){
 
 buttons.forEach(function(a){
 
-if(a.textContent.toLowerCase().search(search.value.toLowerCase()) === -1) {a.classList.add("invalid")}else{a.classList.remove("invalid")}
-console.log(a);
-
-})}
+if(a.textContent.toLowerCase().search(search.value.toLowerCase()) === -1) {
+  a.classList.add("invalid")
+}else{
+  a.classList.remove("invalid")
+}
+}
+)
+}
