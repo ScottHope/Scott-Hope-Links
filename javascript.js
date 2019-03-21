3dfd00494e4c1c4968ea48274ba3d4310e9d8a1d
ScottHope <hughegreen@btinternet.com>
2019-03-21 16:39:07 +0000

    finalised search box styling

--- javascript.js
+++ javascript.js
@@ -1,21 +1,20 @@
 let searchBox = document.querySelector("input");
 let buttons = document.querySelectorAll(".button");
+let match = document.querySelector("#matches");
+let inputBG = document.querySelector(":root");
+// let count = 0;
 searchBox.addEventListener("input", buttonSearch);
+searchBox.focus();
 
-if(sessionStorage.menuSearch === undefined){
-  sessionStorage.setItem("menuSearch","");
-} else {
-  searchBox.value = sessionStorage.menuSearch; buttonSearch();
-}
+sessionStorage.menuSearch === undefined ? sessionStorage.setItem("menuSearch", "") : (searchBox.value = sessionStorage.menuSearch, buttonSearch());
 
-function buttonSearch(){
+function buttonSearch() {
+  count = 0;
   sessionStorage.menuSearch = searchBox.value;
-  buttons.forEach(function(a){
-    if(a.textContent.toLowerCase().search(searchBox.value.toLowerCase()) === -1) {
-      a.classList.add("invalid")
-    }else{
-      a.classList.remove("invalid")
-    }
+  buttons.forEach(function (a) {
+    a.textContent.toLowerCase().search(searchBox.value.toLowerCase()) > -1 ? (a.classList.remove("invalid"), count++) : a.classList.add("invalid");
   }
-  )
+  );
+  (searchBox.value === "" && count === 63) ? match.textContent = "" : match.textContent = "Matches : " + count;
+  count === 0 ? inputBG.style.setProperty("--inputColor","#faa") : inputBG.style.setProperty("--inputColor","");
 }
