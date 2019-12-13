92c2ef25f6798b4dcdbbf97eb0f9d20308472a2f
ScottHope <hughegreen@btinternet.com>
2019-12-13 16:26:34 +0000

    fixed tabbing through disabled buttons

--- docs/scripts/javascript.js
+++ docs/scripts/javascript.js
@@ -2,7 +2,7 @@ let searchBox = document.querySelector("input"),
 buttons = document.querySelectorAll(".button"),
 match = document.querySelector("#matches>span"),
 inputBG = document.querySelector(":root"),
-counter = document.querySelectorAll(".counter img")[0];
+counter = document.querySelectorAll("#counter img")[0];
 
 searchBox.addEventListener("input", buttonSearch);
 searchBox.focus();
@@ -14,11 +14,11 @@ function buttonSearch() {
   count = 0;
   sessionStorage.menuSearch = searchBox.value;
   for (var i = 0; i < buttons.length; i ++){
-    buttons[i].textContent.toLowerCase().search(searchBox.value.toLowerCase()) > -1 ? (buttons[i].classList.remove("invalid"), count++) : buttons[i].classList.add("invalid");
+    buttons[i].textContent.toLowerCase().search(searchBox.value.toLowerCase()) > -1 ? (buttons[i].classList.remove("invalid"), buttons[i].tabIndex = 0, count++) : (buttons[i].classList.add("invalid"), buttons[i].tabIndex = -1);
   }
 
   (searchBox.value === "" && count === buttons.length) ? match.innerHTML = 0 : match.innerHTML = count;
   count === 0 ? inputBG.style.setProperty("--inputColor","#faa") : inputBG.style.setProperty("--inputColor","");
 }
 
-counter.removeAttribute("title");
+//counter.removeAttribute("title");
